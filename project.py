54bc3857bc6e22758678336e9c09c637e2088bc0
yeasirahnaf <yeasir.ahnaf.1997@gmail.com>
2024-12-30 20:29:57 +0600

    BUG: Incorrect multiplication

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 def greet(name): return f"Greetings, {name}!"
 def add(a, b): return a + b
-def multiply(a, b): return a * b
+def multiply(a, b): return a + b  # BUG!
 def divide(a, b): return a / b
 def subtract(a, b): return a - b
 def power(a, b): return a**b
